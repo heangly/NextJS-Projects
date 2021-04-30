145b93e7cd6f046e37e1d99ecf9d6ed4f837572b
Heang Ly <heanglypa@gmail.com>
2021-04-30 11:13:53 -0400

    authorization and access

--- dj-events/fronend/pages/account/dashboard.js
+++ dj-events/fronend/pages/account/dashboard.js
@@ -1,11 +1,47 @@
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
+import { parseCookies } from '../../helpers/index'
+import styles from '@/styles/Dashboard.module.css'
+import DashboardEvent from '@/components/DashboardEvent'
 
-const DashBoardPage = () => {
+const DashBoardPage = ({ events }) => {
+  const handleDeleteEvent = (id) => {
+    console.log(id)
+  }
+
   return (
     <Layout title='User Dashboard'>
-      <h1>Dashboard</h1>
+      <div className={styles.dash}>
+        <h1>Dashboard</h1>
+        <h3>My Events</h3>
+        {events.map((event) => (
+          <DashboardEvent
+            key={event.id}
+            evt={event}
+            handleDeleteEvent={handleDeleteEvent}
+          />
+        ))}
+      </div>
     </Layout>
   )
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req)
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+
+  const events = await res.json()
+
+  return {
+    props: {
+      events
+    }
+  }
 }
 
 export default DashBoardPage
